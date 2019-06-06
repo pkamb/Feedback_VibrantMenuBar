4e060b2e38bea5eeacdc38c40bbfde2353012e6c
Peter Kamb <peterkamb@gmail.com>
2019-06-05 19:46:04 -0700

    Add new Xcode project

---
+++ VibrantMenuBar/VibrantMenuBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VibrantMenuBar
+//
+//  Created by Feedback on 6/5/19.
+//  Copyright © 2019 Feedback. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VibrantMenuBar/VibrantMenuBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VibrantMenuBar
+//
+//  Created by Feedback on 6/5/19.
+//  Copyright © 2019 Feedback. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

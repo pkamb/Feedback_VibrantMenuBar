b6274bd8b9566e25a469d26f1c69ef7b09febefc
Peter Kamb <peterkamb@gmail.com>
2019-06-10 19:21:03 -0700

    add bridging header for private NSMenuItem methods

---
+++ VibrantMenuBar/VibrantMenuBar/VibrantMenuBar-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface NSMenuItem ()
+
+@property (setter=_setViewHandlesEvents:) BOOL _viewHandlesEvents;
+@property (setter=_setViewIsDrawingOnly:) BOOL _viewIsDrawingOnly;
+
+@end

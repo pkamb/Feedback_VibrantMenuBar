e08287df21abfd1f0866f4ec48a1efa264a73680
Peter Kamb <peterkamb@gmail.com>
2019-06-10 22:57:24 -0700

    delete outdated code

--- VibrantMenuBar/VibrantMenuBar/MenuViews.swift
+++ VibrantMenuBar/VibrantMenuBar/MenuViews.swift
@@ -51,27 +51,6 @@ enum MenuViews: CaseIterable {
         return menuItem
     }
     
-    static func menuViews() -> [NSMenuItem] {
-        
-        let menuData = [
-            ("normal", NSColor.clear)
-        ]
-        
-        let menuItems: [NSMenuItem] = menuData.map({
-            let view = HighlightView(frame: NSRect(x: 0, y: 0, width: 300, height: 50))
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.addConstraint(NSLayoutConstraint(item: view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: 50))
-            
-            let label = NSTextField(labelWithString: $0.0)
-            view.addArrangedSubview(label)
-            
-            let menuItem = NSMenuItem(title: "", action: nil, keyEquivalent: "")
-            menuItem.view = view
-            return menuItem
-        })
-        
-        return menuItems
-    }
 }
 
 class HighlightView: NSStackView {
